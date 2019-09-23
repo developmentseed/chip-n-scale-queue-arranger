86af1c38a8b40e2d10351595b3bc14bc9ea238a0
Mark Wronkiewicz <wronk@developmentseed.org>
2019-09-23 10:37:10 -0700

    Add class index option and clean up

--- scripts/csv_to_geojson.py
+++ scripts/csv_to_geojson.py
@@ -7,12 +7,9 @@ you ran prediction over a large area -- it will keep the resulting geojson
 much smaller on disk. Set the threshold to 0.0 to include all tile predictions
 in the geojson output.
 
-python csv_to_geojson.py my_results_file.csv 0.75
-
 Requires pygeotile, geojson, and mercantile
 """
 
-import sys
 import csv
 import json
 import argparse
@@ -23,7 +20,7 @@ from geojson import Feature
 from pygeotile.tile import Tile as Pygeo_tile
 
 
-def convert_csv(fname_csv, fname_geojson, tile_format, thresh):
+def convert_csv(fname_csv, fname_geojson, tile_format, thresh_ind, thresh):
     """Convert tile indices in CSV file to geojson"""
 
     if not op.exists(fname_csv):
@@ -37,6 +34,9 @@ def convert_csv(fname_csv, fname_geojson, tile_format, thresh):
     else:
         raise ValueError(f'Tile format not understood. Got: {tile_format}')
 
+    if not 0 <= thresh <= 1.:
+        raise ValueError(f"'thresh' must be on interval [0, 1]. Got: {thresh}")
+
     with open(fname_csv, 'r') as csvfile:
         with open(fname_geojson, 'w') as results:
             reader = csv.reader(csvfile)
@@ -46,11 +46,10 @@ def convert_csv(fname_csv, fname_geojson, tile_format, thresh):
             results.write('{"type":"FeatureCollection","features":[')
             next(reader)  # Skip header
 
-            for ri, row in enumerate(reader):
+            for row in reader:
 
                 # Load as pygeotile using TMS coords
                 geot = tile_func(*[int(t) for t in row[0].split('-')])
-                #google_coords = geot.google  # Convert to X/Y
 
                 # Create feature with mercantile
                 feat = feature(Tile(geot.google[0], geot.google[1], geot.zoom))
@@ -58,14 +57,15 @@ def convert_csv(fname_csv, fname_geojson, tile_format, thresh):
                 # Get class prediction confidences
                 pred = json.loads(','.join(row[1:]))
                 pred_red = list(map(lambda x: round(x, 2), pred))
-                if pred_red[1] >= float(thresh):
+                if pred_red[thresh_ind] >= thresh:
                     # Add commas prior to any feature that isn't the first one
                     if first_line:
                         first_line = False
                     else:
                         results.write(',')
 
-                    pred_obj = dict(zip(map(lambda x: 'p%s' % x, range(len(pred_red))), pred_red))
+                    pred_obj = dict(zip(map(lambda x: 'p%s' % x,
+                                            range(len(pred_red))), pred_red))
 
                     results.write(json.dumps(Feature(geometry=feat['geometry'],
                                                      properties=pred_obj)))
@@ -77,10 +77,17 @@ def convert_csv(fname_csv, fname_geojson, tile_format, thresh):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Convert CSV of tile predictions to GeoJSON.')
-    parser.add_argument('fname_csv', type=str, help='Filepath to CSV file needing conversion.')
-    parser.add_argument('fname_geojson', type=str, default='results.geojson', help='Filepath to save geojson file to.')
-    parser.add_argument('--tile_format', type=str, default='tms', help='Format of tile indices in CSV file ("tms" or "google").')
-    parser.add_argument('--thresh', type=str, default='0', help='Optional threshold for including a prediction.')
+    parser.add_argument('fname_csv', type=str,
+                        help='Filepath to CSV file needing conversion.')
+    parser.add_argument('fname_geojson', type=str, default='results.geojson',
+                        help='Filepath to save geojson file to.')
+    parser.add_argument('--tile-format', type=str, default='tms',
+                        help='Format of tile indices in CSV file ("tms" or "google").')
+    parser.add_argument('--thresh-ind', type=int, default=1,
+                        help='Optional threshold class index for including a prediction.')
+    parser.add_argument('--thresh', type=float, default=0.,
+                        help='Optional threshold for including a prediction.')
 
     args = parser.parse_args()
-    convert_csv(args.fname_csv, args.fname_geojson, args.tile_format, args.thresh)
+    convert_csv(args.fname_csv, args.fname_geojson, args.tile_format,
+                args.thresh_ind, args.thresh)
