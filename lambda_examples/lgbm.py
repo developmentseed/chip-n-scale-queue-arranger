54b0c0d84ebb2d0ffd9ae6533926afa945a83b3e
Drew Bollinger <drew@developmentseed.org>
2020-10-23 15:57:13 -0400

    add latest lambda

--- lambda_examples/lgbm.py
+++ lambda_examples/lgbm.py
@@ -9,6 +9,8 @@ import os
 import pg8000
 from typing import Dict, Any, List, Tuple
 from io import BytesIO
+import logging
+import traceback
 
 from PIL import Image
 import numpy as np
@@ -26,29 +28,43 @@ from download_and_predict.custom_types import SQSEvent
 SHAPE = (256, 256) # this should probably be defined somewhere else
 THRESHOLD = 0.95
 
+logging.getLogger("botocore.credentials").disabled = True
+logging.getLogger("botocore.utils").disabled = True
+
 class LGBMDownloader(DownloadAndPredict):
-    def __init__(self, imagery: str, db: str, prediction_endpoint: str, sentinel_wms_kwargs: Dict[str, Any]):        # type annotatation error ignored, re: https://github.com/python/mypy/issues/5887
-        super(DownloadAndPredict, self).__init__(dict( # type: ignore
-            imagery=imagery,
-            db=db,
-            prediction_endpoint=prediction_endpoint
-        ))
-        self.sentinel_wms_kwargs = sentinel_wms_kwargs
+    def __init__(self, imagery: str, db: str, prediction_endpoint: str):        # type annotatation error ignored, re: https://github.com/python/mypy/issues/5887
+        super(DownloadAndPredict, self).__init__()
+        self.imagery = imagery
+        self.db = db
+        self.prediction_endpoint = prediction_endpoint
 
     def get_images(self, tiles, layer: str):
+        NDVI = "(B08 - B04) / (B08 + B04)"
+        SAVI = "1.5 * (B08-B04) / (0.5 + B08 + B04)"
+        
         for tile in tiles:
-            NDVI = "(B08 - B04) / (B08 + B04)"
-            SAVI = "1.5 * (B08-B04) / (0.5 + B88 + B04)"
-            with MosaicBackend(layer, reader=S2COGReader,) as src_dst:
-                (data, _), _ = src_dst.tile(
-                    tile.x,
-                    tile.y,
-                    tile.z,
-                    pixel_selection=MedianMethod(),
-                    tilesize=256,
-                    expression=f"B02,B8A,B11,B12,{NDVI},{SAVI}"
-                )
-                yield (tile, reshape_as_image(data))
+            # in the order of Carole King's, "You've Got a Friend"
+            data = list()
+            for season in ['winter', 'spring', 'summer', 'autumn']:
+                season_layer = layer.replace('{season}', season)
+                try:
+                    with MosaicBackend(season_layer, reader=S2COGReader,) as src_dst:
+                        (season_data, _), _ = src_dst.tile(
+                            tile.x,
+                            tile.y,
+                            tile.z,
+                            pixel_selection=MedianMethod(),
+                            tilesize=256,
+                            expression=f"B02,B8A,B11,B12,{NDVI},{SAVI}"
+                        )
+                        data.append(reshape_as_image(season_data))
+                except Exception:
+                    traceback.print_exc()
+                    print(f'error: skipping {season}')
+                    data.append(np.zeros((256, 256, 6)))
+
+            yield (tile, np.reshape(np.stack(data, axis=2), (256 * 256, 24)))
+                
 
     def get_prediction_payload(self, tiles: List[Tile], layer) -> Tuple[List[Tile], str]:
         """
@@ -65,7 +81,7 @@ class LGBMDownloader(DownloadAndPredict):
         tiles_and_images = self.get_images(tiles, layer)
         tile_indices, images = zip(*tiles_and_images)
 
-        payload = str([img.reshape(SHAPE[0] * SHAPE[1], img.shape[-1]).tolist() for img in images])
+        payload = str([img.reshape(SHAPE[0] * SHAPE[1], img.shape[-1]).tolist() for img in images][0])
 
         return (list(tile_indices), payload)
 
@@ -89,7 +105,7 @@ def handler(event: SQSEvent, context: Dict[str, Any]) -> None:
     assert(prediction_endpoint)
 
     # instantiate our DownloadAndPredict class
-    dap = DownloadAndPredict(
+    dap = LGBMDownloader(
       imagery=imagery,
       db=db,
       prediction_endpoint=prediction_endpoint
@@ -97,7 +113,6 @@ def handler(event: SQSEvent, context: Dict[str, Any]) -> None:
 
     # get tiles from our SQS event
     tiles = dap.get_tiles(event)
-    print(tiles, imagery)
 
     # construct a payload for our prediction endpoint
     tile_indices, payload = dap.get_prediction_payload(tiles, imagery)
@@ -108,6 +123,6 @@ def handler(event: SQSEvent, context: Dict[str, Any]) -> None:
     # save prediction request to db
     dap.save_to_db(
         tile_indices,
-        content,
+        [content],
         result_wrapper=prediction_to_image
     )
